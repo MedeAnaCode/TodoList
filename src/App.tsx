6a38de5f5bce45eb6c891a35812eec6961bf71cf
MedeAnaCode <medentsova.anastasia@yandex.ru>
2025-09-29 17:38:47 +0300

    Enables local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { v4 as uuidv4 } from 'uuid';
 import Filter from "./Filter.tsx";
 import TodoList from "./TodoList.tsx";
 import TodoForm from "./TodoForm.tsx";
 import type { Task, FilterPattern } from "./types";
 
+const LS_KEY_TASKS = 'tasks';
+const LS_KEY_FILTER = 'filter';
+
 function App() {
-    const [tasks, setTasks] = useState<Task[]>([]);
-    const [filter, setFilter] = useState<FilterPattern>('all');
+    const [tasks, setTasks] = useState<Task[]>(() => {
+        try {
+            const savedTasks = localStorage.getItem(LS_KEY_TASKS);
+            return savedTasks ? (JSON.parse(savedTasks) as Task[]) : [];
+        } catch {
+            return [];
+        }
+    });
+
+    const [filter, setFilter] = useState<FilterPattern>(() => {
+        const savedFilter = localStorage.getItem(LS_KEY_FILTER);
+        return savedFilter === "all" || savedFilter === "active" || savedFilter === "done" ? savedFilter : "all";
+    });
+
+    useEffect(() => {
+        localStorage.setItem(LS_KEY_TASKS, JSON.stringify(tasks));
+    }, [tasks]);
+
+    useEffect(() => {
+        localStorage.setItem(LS_KEY_FILTER, filter);
+    }, [filter]);
 
     const onAdd = (textMessage: string) => {
         const newTask: Task = {
